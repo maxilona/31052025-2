bd32c43264daf7c706e5e3befbbdb5513514bf64
mgvrez <maxilona2024@gmail.com>
2025-06-01 00:46:39 +0300

    src

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,24 @@
 public class Main {
     public static void main(String[] args) {
 
-        
+        int amount = 100;
+        int deposit = 2000;
+        int bonus = deposit / 100;
+        int totalAmount = amount + deposit;
 
+        if (deposit > 1000) {
+            totalAmount = amount + deposit + bonus;
+        } else {
+            totalAmount = amount + deposit;
         }
+        System.out.println("Первоначальный размер счета:" + " " + amount + " " + "рубль/ей");
+        System.out.println("Сумма пополнения:" + " " + deposit + " " + "рубль/ей");
+        if (deposit > 1000) {
+            System.out.println("Размер бонуса:" + " " + bonus + " " + "рубль/ей");
+        }
+        System.out.println("Итоговый размер счета:" + " " + totalAmount + " " + "рубль/ей");
+        ;
+
+
     }
 }
